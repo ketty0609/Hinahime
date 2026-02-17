552dc4a26359589b2ed214a77711e26679393d4c
ketty0609 <buloveteng13@gmail.com>
2026-02-18 02:22:28 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated');
-});
-
-self.addEventListener('fetch', event => {});
